7a0e8101d0a71f05e860dca0bc42f5f13f1a2f7d
jhonatan soto <jhonrocker2012@gmail.com>
2021-06-08 22:29:17 -0400

    exercise 1.13 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM, { render } from 'react-dom'
+
+const Display = ({points, anecdotes}) => {
+  const max = Math.max(...points)
+  const position = points.indexOf(max)  
+  return(
+    <>
+    <h1>{anecdotes[position]}</h1>
+    </>
+  )
+}
+
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(5).fill(0))
+  
 
   const randomNumber = () => {
     const random = Math.floor(Math.random() * 5)
     setSelected(random)
   }
 
+  const suma = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h2>Anecdote of the day</h2>
+      <p>{props.anecdotes[selected]}</p>
       <br />
       <button onClick = {() => randomNumber()}>next anecdote</button>
+      <button onClick = {() => suma()}>Vote</button>
+      <h2>Anecdote with most votes</h2>
+      <Display points = {points} anecdotes = {props.anecdotes} />
     </div>
   )
 }
