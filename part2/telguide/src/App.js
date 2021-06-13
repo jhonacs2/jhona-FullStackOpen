6bcaf05259be53abbaf7c6a864f0fcebdda9fd97
jhonatan soto <jhonrocker2012@gmail.com>
2021-06-13 17:36:25 -0400

    telephoneGuide exercise 2.6

---
+++ part2/telguide/src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import Numbers from "./components/Numbers"
+const App = () => {
+  const [persons , setPersons ] = useState([
+    {name:'Jhonatan Soto'}
+  ])
+  const [newName, setnewName] = useState('')
+
+  const handleNameChange = (e) => {
+    setnewName(e.target.value)
+  }
+
+  const addNumber = (e) => {
+    e.preventDefault()
+    const newContact = {
+      name: newName
+    }
+    setPersons(persons.concat(newContact))
+  }
+
+  return(
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map( (person, index) => {
+        return <Numbers key = {index} person = {person}/>
+      })}
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ part2/telguide/src/components/Numbers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Numbers = ({ person }) => {
+    // console.log(person)
+    return(
+        <>
+        <p>{person.name}</p>
+        </>
+        
+    )
+}
+
+export default Numbers
