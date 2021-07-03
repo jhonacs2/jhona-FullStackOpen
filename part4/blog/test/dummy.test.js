cbeb8df4bd869fd3f09602bc85439f352c3490dd
Jhonatan Soto <jhonrocker2012@gmail.com>
2021-07-03 19:34:20 -0400

    blog exercises 4.1 4.2 4.3 4.4 4.5

---
+++ part4/blog/test/dummy.test.js
@@ -0,0 +1,71 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+  
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+  })
+
+
+  describe('favoriteBlog is', () => {
+    const listBlogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Pokemon',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 15,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To TF2',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 6,
+        __v: 0
+      }
+
+    ]
+
+    const correctAnswer = {
+        title: 'Go To Pokemon',
+        author: 'Edsger W. Dijkstra',
+        likes: 15
+    }
+  
+    test('blog more popular', () => {
+      const result = listHelper.favoriteBlog(listBlogs)
+      expect(result).toEqual(correctAnswer)
+    })
+  })
+
+  
+
---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,26 @@
+const dummy = (blogs) => {
+    return 1 
+}
+
+const totalLikes = (likes) => {
+  return (likes.length === 1) 
+  ? likes[0].likes
+  : likes.reduce((accumulator,currentValue) => {
+      return accumulator + currentValue.likes
+  },0) 
+}
+
+const favoriteBlog = (Tlikes) => {
+  
+  const {title,author,url,likes} = Tlikes.reduce((accumulator,currentValue) => {
+      return (accumulator.likes > currentValue.likes) ? accumulator : currentValue
+  },0)
+   return {title,author,likes}
+}
+  
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
+
