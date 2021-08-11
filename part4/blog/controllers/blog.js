3bf9f6f25f474a053b938d8cb8de02ea69fb604a
Jhonatan Soto <jhonacs@DESKTOP-6H0QMCB.localdomain>
2021-08-11 19:54:50 -0400

    exercises 4.13 4.14

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -6,6 +6,15 @@ blogRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
+blogRouter.get("/:id",async(request,response) => {
+  const blog = await Blog.findById(request.params.id)
+  if(blog){
+    response.status(200).json(blog)
+  }else{
+    response.status(404).end()
+  }
+})
+
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
   const blog = new Blog({
@@ -18,5 +27,21 @@ blogRouter.post("/", async (request, response) => {
   const savedBlog = await blog.save();
   response.status(200).json(savedBlog);
 });
+
+blogRouter.put("/:id", async(request, response) => {
+  const {likes} = request.body
+
+  const updateBlog = {
+    likes
+  }
+
+  const newBlog = await Blog.findByIdAndUpdate(request.params.id,updateBlog, { new: true })
+  if(newBlog){
+    response.status(200).json({newBlog})
+  }else{
+    response.status(404).end()
+  }
+  
+})
 
 module.exports = blogRouter;
