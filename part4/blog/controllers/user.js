1e09cf96985f2e50d6431db19a1a0adcfd59ba0a
Jhonatan Soto <jhonrocker2012@gmail.com>
2021-08-18 21:40:22 -0400

    exercise 4.18 - 4,19

--- part4/blog/controllers/user.js
+++ part4/blog/controllers/user.js
@@ -2,18 +2,22 @@ const bcrypt = require("bcrypt");
 const userRouter = require("express").Router();
 const User = require("../models/user");
 
-userRouter.get('/',async (request,response) => {
-    const users = await User.find({})
-    response.json(users)
-})
-
+userRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
 
 userRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
-  console.log((username.length && password.length) < 3)
-    if((username.length && password.length) < 3){
-        return response.status(400).json({ error: "username or password is shorter than minimun allowed length (3)"})
-    }
+  console.log((username.length && password.length) < 3);
+  if ((username.length && password.length) < 3) {
+    return response
+      .status(400)
+      .json({
+        error:
+          "username or password is shorter than minimun allowed length (3)",
+      });
+  }
   const saltRounds = 10;
   const passwordHash =
     password === undefined ? false : await bcrypt.hash(password, saltRounds);
--- part4/blog/models/user.js
+++ part4/blog/models/user.js
@@ -14,6 +14,12 @@ const userSchema = new mongoose.Schema({
   passwordHash: {
       type:String
   },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog"
+    }
+  ]
 });
 
 userSchema.set("toJSON", {
